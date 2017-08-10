94f166b58ece8343bed5c7a318fe1e946c798bc6
cdonatom <carlosdonato90@gmail.com>
2017-08-10 15:18:34 +0200

    Reworked IPTABLES script

--- add_iptables.sh
+++ add_iptables.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+if [ "$#" -lt 1 ];then
+    echo "Execute as: $0 <dev>"
+    exit 1
+fi
+
 echo "Enabling IPTABLES rules"
-sudo iptables -t nat -A POSTROUTING -s 172.21.20.0/24 -o eth0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 172.21.20.0/24 -o $1 -j MASQUERADE
 echo "Enabling IPv4 forwarding for network interfaces"
 sudo sysctl -w net.ipv4.ip_forward=1
 
