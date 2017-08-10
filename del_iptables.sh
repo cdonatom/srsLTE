b2875232f2fa57c9154a83347dfbd1cbc10abc6d
cdonatom <carlosdonato90@gmail.com>
2017-08-10 15:21:12 +0200

    Reworked IPTABLES scripts

--- del_iptables.sh
+++ del_iptables.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+
+if [ "$#" -lt 1 ];then
+    echo "Execute as: $0 <dev>"
+    exit 1
+fi
+
 echo "Disabling IPTABLES rules"
-sudo iptables -t nat -D POSTROUTING -s 172.21.20.0/24 -o eth0 -j MASQUERADE
+sudo iptables -t nat -D POSTROUTING -s 172.21.20.0/24 -o $1 -j MASQUERADE
 echo "Disabling IPv4 forwarding for network interfaces"
 sudo sysctl -w net.ipv4.ip_forward=0
